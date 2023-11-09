34988878096a8de2bf33095d458b4d5cd560277b
manggury <148739312+manggury@users.noreply.github.com>
2023-11-09 18:14:13 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -47,4 +47,36 @@ $(function () {
     $('.mainEvet .arrows .right').on('click', function () {
         $('.event_img_box').slick('slickNext');
     });
+
+    // $('.social_tab li a.youtube').on('click', function (e) {
+    //     e.preventDefault();
+    //     const idx = $(this).parent().index();
+    //     console.log(idx);
+    //     $('.tab_menu .youtube_menu').removeClass('on');
+    //     $('.tab_menu .youtube_menu').eq(idx).addClass('on');
+
+    //     $('.tab_menu').hide();
+    // })
+
+    $('.social_tab li a').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).parent().index();
+        console.log(idx);
+        $('.tab_menu .content').removeClass('on');
+        $('.tab_menu .content').eq(idx).addClass('on');
+
+        //$('.tab_menu').hide();
+    });
+
+    $('.FB_iframe').slick({
+        slidesToShow: 1,
+        arrows: false,
+    })
+
+    $('.facebook_menu .arrows .left').on('click', function () {
+        $('.FB_iframe').slick('slickPrev');
+    })
+    $('.facebook_menu .arrows .right').on('click', function () {
+        $('.FB_iframe').slick('slickNext');
+    });
 });
